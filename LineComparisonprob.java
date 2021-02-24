8f3cc9b9a933976170c0e54a195ff51f7ed2e4a2
unknown <naikmahesh.naik@gmail.com>
2021-02-24 23:37:00 +0530

    lines are equal or not

--- LineComparisonprob.java
+++ LineComparisonprob.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 class LineComparisonProb{
         public static void main(String[] args){
+		float x1,y1,x2,y2;
+		double length;
 
-                float x1,y1,x2,y2;
-                double length;
+                float x3,y3,x4,y4;
+                double length1,length2;
                 System.out.println("Welcome to Line Comparison Problems.");
 
                 Scanner sc = new Scanner(System.in);
@@ -20,7 +22,30 @@ class LineComparisonProb{
 
                 length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
                 System.out.println("\nLength of the line : "+ length);
-        }
+		System.out.println("\n Enter x3 co-ordinate of the line.");
+      x3 = sc.nextFloat();
+      System.out.println("\n Enter y3 co-ordinate of the line.");
+      y3 = sc.nextFloat();
+
+		System.out.println("\n Enter x4 co-ordinate of the line.");
+      x4 = sc.nextFloat();
+      System.out.println("\n Enter y4 co-ordinate of the line.");
+      y4 = sc.nextFloat();
+
+		System.out.println("\n Coordinates of the line1 is x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
+ 		System.out.println("\n Coordinates of the line2 is x3="+x3+ " y3="+y3+" and x4="+x4 + " y4="+y4);
+
+
+		length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+		length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
+
+      if(length1==length2)
+			System.out.println("\n The lines are Equal. ");
+		else
+         System.out.println("\n The lines are not Equal. ");        
+
+
+}
 }
 
 
