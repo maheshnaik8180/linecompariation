faa4b1e20a3e9c590207612c0525a1482bf18391
unknown <naikmahesh.naik@gmail.com>
2021-02-24 23:23:48 +0530

    add two point

---
+++ LineComparisonprob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class LineComparisonProb{
+        public static void main(String[] args){
+
+                float x1,y1,x2,y2;
+                double length;
+                System.out.println("Welcome to Line Comparison Problems.");
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("\n Enter x1 co-ordinate of the line.");
+                x1 = sc.nextFloat();
+      System.out.println("\n Enter y1 co-ordinate of the line.");
+      y1 = sc.nextFloat();
+
+                System.out.println("\n Enter x2 co-ordinate of the line.");
+      x2 = sc.nextFloat();
+      System.out.println("\n Enter y2 co-ordinate of the line.");
+      y2 = sc.nextFloat();
+      System.out.println("\n Coordinates of the line are x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
+
+                length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+                System.out.println("\nLength of the line : "+ length);
+        }
+}
+
+
